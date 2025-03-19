a15a198eb9e226cb17993d337795a7de123015ec
AMINE EL QARDAOUI <el-q0003@2M1U2-03S02516.ad-urca.univ-reims.fr>
2025-03-19 18:06:55 +0100

    TP3 JS

---
+++ TP3/script.js
@@ -0,0 +1,26 @@
+const champ = document.getElementById(inputDateDebut)
+, champ2 = document.getElementById(inputDateFin)
+, regex = (/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/)
+
+
+function checkFormat(champ){
+    if (champ.value.match(regex)){
+        console.log("format ok")
+        if (moment(champ.value).isValid()){
+            checkValidity()
+        }
+        else{
+            console.log("Non c pas bon");
+            
+        }
+    }
+    else{
+        console.log("non");
+    }
+
+
+}
+
+function checkValidity(champ){
+    
+}
